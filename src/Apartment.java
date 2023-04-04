0b720a9db5c7c01c9ad586a067b08eb590893e0b
users <kalmatovazarina1@gmail.com>
2023-04-04 22:08:05 +0600

    zarina commit

---
+++ src/Apartment.java
@@ -0,0 +1,7 @@
+public class Apartment extends Famile implements PaymentAble{
+    @Override
+    public void summa() {
+        System.out.println("коммунальные услуги за месяц : 2500 сом ");
+    }
+
+}
